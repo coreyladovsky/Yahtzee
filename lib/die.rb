6b50a293c564fefd929b323c72602580cb408339
Corey Ladovsky <coreyskymusic@gmail.com>
2018-02-13 13:06:58 -0500

    complete die initialize and <=>

--- lib/die.rb
+++ lib/die.rb
@@ -1,6 +1,20 @@
 class Die
-  attr_reader: :value 
-  def initialize(value = nil)]
+  attr_reader :value
+
+  def initialize(value = nil)
+    raise 'Out of Bounds!' unless value.nil? || value.between?(1, 6)
     @value = value
   end
+
+  def <=>(die)
+    raise "Not a valid Comparison" unless die.is_a? Die
+    if die.value == self.value
+      0
+    elsif die.value < self.value
+      1
+    else
+      -1
+    end
+  end
+
 end
--- spec/die_spec.rb
+++ spec/die_spec.rb
@@ -4,13 +4,36 @@ require 'die'
 describe Die do
   describe '#initialize' do
     subject(:die) { Die.new }
+    subject(:die2) { Die.new(3) }
     it 'gives a die a nil value unless given one' do
       expect(die.value).to eq(nil)
     end
 
-    subject(:die) { Die.new(3) }
     it 'gave a die the correct value when given one' do
-      expect(die.value).to eq(3)
+      expect(die2.value).to eq(3)
+    end
+
+    it 'raises an error with an invaled value' do
+      expect { Die.new(0) }.to raise_error('Out of Bounds!')
+      expect { Die.new(7) }.to raise_error('Out of Bounds!')
+    end
+  end
+
+  describe '#<=>' do
+    it 'raises an error when compared to a non die object' do
+        expect { Die.new(3) <=> 4 }.to raise_error('Not a valid Comparison')
+    end
+
+    it 'should return 0 when die are the same' do
+      expect(Die.new(3) <=> Die.new(3)).to eq(0)
+    end
+
+    it 'should return 1 when die has higher value' do
+      expect(Die.new(6) <=> Die.new(4)).to eq(1)
+    end
+
+    it 'should return -1 when die has lower value' do
+      expect(Die.new(1) <=> Die.new(4)).to eq(-1)
     end
   end
 end
