a432932fc4ab7f7dedb4bf3ac0b6374856fdc9d7
Corey Ladovsky <coreyskymusic@gmail.com>
2018-02-16 17:02:06 -0500

    switch players, take turn

--- lib/game.rb
+++ lib/game.rb
@@ -5,11 +5,11 @@ require_relative 'human_player'
 class Game
 
   def initialize(players)
-    @dice = { 1 => Die.new,
-              2 => Die.new,
-              3 => Die.new,
-              4 => Die.new,
-              5 => Die.new }
+    @dice = { "a" => Die.new,
+              "b" => Die.new,
+              "c" => Die.new,
+              "d" => Die.new,
+              "e" => Die.new }
     @players = players
     @current_player = @players[0]
   end
@@ -20,8 +20,41 @@ class Game
   end
 
   def play
-    @current_player.board.display
+    welcome_message
+    while !game_over
+      take_turn
+      switch_players!
+    end
 
+  end
+
+  def take_turn
+    puts "It's #{@current_player.name} turn"
+    puts "Your score card looks like this"
+    @current_player.board.display
+    puts "Here are the results of your first die roll"
+    roll_die(@dice.valuesß)
+    display_all_die
+    puts "select the die by name, seperated by commas"
+
+  end
+
+  def roll_die(dice)
+    dice.each(&:roll)
+  end
+
+  def display_all_die
+    @dice.each do |key, die|
+      puts "#{key} | #{die.value}"
+    end
+  end
+
+  def game_over
+    @players.all?(&:game_over?)
+  end
+
+  def welcome_message
+    puts "Hello and Welcome to Yatzee"
   end
 
 
--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,9 +1,20 @@
 class HumanPlayer
 
-  attr_reader :name, :board 
+  attr_reader :name, :board
   def initialize(name="Corey")
     @name = name
     @board = Board.new
+    @turns = 13
   end
+
+  def finish_turn
+    @turns -= 1
+  end
+
+  def game_over?
+    @turns == 0
+  end
+
+
 
 end
